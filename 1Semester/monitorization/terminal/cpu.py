7275f7ab666ef91aec96bf7e6aab70f0689b670d
Wellerson <wellerson.coffee@gmail.com>
2020-05-18 09:56:14 -0300

    monitorization computer with python

---
+++ 1Semester/monitorization/terminal/cpu.py
@@ -0,0 +1,6 @@
+import platform
+
+print(f'bits do processador: {platform.processor()}')
+print(f'nome do computador: {platform.node()}')
+print(f'plataforma: {platform.platform()}')
+print(f'sistema operacional: {platform.system()}')
